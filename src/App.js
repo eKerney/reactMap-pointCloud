f6a36d634062e5d7b76cd9de63820fe8ac109588
eKerney <Eric.Kerney@airspacelink.com>
2022-04-08 11:24:37 -0400

    added hex data

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import {PointCloudLayer} from '@deck.gl/layers';
 import {COORDINATE_SYSTEM, OrbitView, LinearInterpolator} from '@deck.gl/core';
 import {LASLoader} from '@loaders.gl/las';
 import {load, registerLoaders} from '@loaders.gl/core';
+import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
+import {scaleThreshold} from 'd3-scale';
+
 registerLoaders([LASLoader]);
 
 const HEX_DATA = "https://raw.githubusercontent.com/chriszrc/foss4g-2021-react-mapbox/main/deck-layers-map/public/data/hex_radio_coverage.json";
@@ -22,6 +25,7 @@ const stationData = 'https://raw.githubusercontent.com/eKerney/reactMapTest/main
 const testData = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
 const lazData = 'https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects/MI_WayneCo_2009/laz/MI_WayneCo_2009_000883.laz'
 const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2018_wildfire_ql2_Job716930.las'
+const weatherData = 'https://raw.githubusercontent.com/eKerney/reactMap-pointCloud/main/data/weatherPolyData.json';
 
   export default function App() {
 
@@ -52,7 +56,6 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
       // visible: true,
       // wrapLongitude: false,
     });
-
     const pointCloudData = new PointCloudLayer({
       id: 'PointCloudLayer',
       data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/pointcloud.json',
@@ -74,23 +77,6 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
       // visible: true,
       // wrapLongitude: false,
     });
-    
-    const layers = [
-     new HexagonLayer({
-      id: "H3 Radio Sources",
-      data: HEX_DATA,
-      pickable: true,
-      wireframe: false,
-      filled: true,
-      extruded: true,
-      elevationScale: 10000,
-      getHexagon: (d) => d.hex,
-      getFillColor: (d) => [255, (1 - d.count / 50) * 255, 0],
-      getElevation: (d) => d.count,
-      opacity: 0.2,
-    })
-    ];
-
     // station point locations layer
     const noaaStations = new ScatterplotLayer({
       id: 'scatterplot-layer',
@@ -114,7 +100,6 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
       //onHover: (info, event) => console.log('Hovered:', info.object),
       //onClick: (info, event) => onClick(info)
     });  
-
     // specs for hex aggrgation layer
     const colorRange = [
       [1, 152, 189],
@@ -132,7 +117,6 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
     };
     const radius = 60000, upperPercentile = 100, coverage = 1;
     // end of hex specs
-
     const stationsHex = [ 
       new HexagonLayer({
         id: 'heatmap',
@@ -157,28 +141,88 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
       noaaStations,
      ];
 
+     const radioHex = new HexagonLayer({
+      id: "H3 Radio Sources",
+      data: HEX_DATA,
+      pickable: true,
+      wireframe: false,
+      filled: true,
+      extruded: true,
+      elevationRange: [0, 3000],
+      elevationScale: 1000,
+      getPosition: (d) => d.hex,
+      getFillColor: (d) => [255, (1 - d.count / 50) * 255, 0],
+      getElevation: (d) => d.count,
+      opacity: 0.2,
+    });
+
+    const radioHex2 = new H3HexagonLayer({
+      id: "H3 Radio Sources",
+      data: HEX_DATA,
+      pickable: true,
+      wireframe: false,
+      filled: true,
+      extruded: true,
+      elevationScale: 10000,
+      getHexagon: (d) => d.hex,
+      getFillColor: (d) => [255, (1 - d.count / 50) * 255, 0],
+      getElevation: (d) => d.count,
+      opacity: 0.5,
+    });
+
+    const weatherHex = new H3HexagonLayer({
+      id: "weatherDataHex",
+      data: 'https://raw.githubusercontent.com/eKerney/reactMap-pointCloud/main/data/weatherPolyData.json',
+      pickable: true,
+      wireframe: false,
+      filled: true,
+      extruded: true,
+      elevationScale: 10000,
+      getHexagon: (d) => d.properties.h3_index,
+      getFillColor: [255, 100, 100],
+      getElevation: 10000,
+      opacity: 0.5,
+    });
+
+     const weatherGeo1 = new GeoJsonLayer({
+        id: 'geojson1',
+        data: 'https://raw.githubusercontent.com/eKerney/reactMap-pointCloud/main/data/weatherPolyData.json',
+        opacity: 0.7,
+        stroked: false,
+        filled: true,
+        extruded: true,
+        wireframe: true,
+        getElevation: d => ((d.properties.windSpeed * d.properties.precipChance)/d.properties.temperature)**6 * 1000,
+        getFillColor: d => [(500 - (d.properties.temperature**4/100000)), 
+        ((d.properties.precipChance**4/100)), 255], 
+        getLineColor: [255, 255, 255],
+        pickable: true
+      });
+    const weatherGeo2 =
+      new GeoJsonLayer({
+        id: 'geojson2',
+        data: 'https://raw.githubusercontent.com/eKerney/reactMap-pointCloud/main/data/weatherPolyData.json',
+        opacity: 0.1,
+        stroked: false,
+        filled: false,
+        extruded: true,
+        wireframe: true,
+        getElevation: d => ((d.properties.windSpeed * d.properties.temperature)/d.properties.precipChance) * 200,
+        getFillColor: d => [(500 - (d.properties.temperature**4/100000)), ((d.properties.precipChance**4/100)), 0], 
+        getLineColor: [255, 255, 255],
+        pickable: true
+      });
+
+    //const layers = [weatherGeo1, weatherGeo2, pointCloudData, radioHex];
+    const layers = [weatherHex];
     const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZXJpY2tlcm5leSIsImEiOiJja2FjbTNiMXcwMXp1MzVueDFjdDNtcW92In0.LW0qdB-2FmA3UK51M67fAQ';
-    
     const [viewState, setViewState] = React.useState({
-      longitude: -97,
-      latitude: 37,
-      zoom: 4,
+      longitude: -97.5,
+      latitude: 27.8,
+      zoom: 10,
       pitch: 60,
       bearing: -60,
     });
-
-    const [viewState2, setViewState2] = React.useState({
-      longitude: -122.4,
-      latitude: 37.74,
-      zoom: 13,
-      maxZoom: 20,
-      pitch: 60,
-      bearing: 0
-    });
-    
-  const [hoverInfo, setHoverInfo] = useState();  
-  const deckRef = useRef(null);
-
   const getTooltip = info => {
     if (!info.object) {
       return null;
@@ -188,13 +232,8 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
     ${info.object.id}
     ${info.object.elevation} elev m.`;
   };
-  const onClick = useCallback(e => {
-    setFeatures(currFeatures => {
-    console.log(e);  
-    return e;
-    });
-  }, []);
-
+  const [hoverInfo, setHoverInfo] = useState();  
+  const deckRef = useRef(null);
   const [selected, setSelected] = useState(null);
   const [toggle, setToggle] = useState(false);
   const [showPopup, setShowPopup] = React.useState(true);
@@ -202,12 +241,11 @@ const lasData = 'https://github.com/eKerney/reactMap-pointCloud/raw/main/src/ca2
 
   return (
     <>
-
     <DeckGL 
       ref={deckRef}
       initialViewState={viewState} 
       controller={true}
-      layers={[pointCloudData]}
+      layers={[...layers]}
       ContextProvider={MapContext.Provider}
       onClick={({ x, y, coordinate, object}) => {
         // TODO: figure out how to get rid of extra click event
